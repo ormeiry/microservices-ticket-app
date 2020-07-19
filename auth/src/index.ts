df4edbfb528ea16d923c024227e38e0e279e4f23
ormeiry <ormeiry2411@gmail.com>
2020-07-19 13:08:07 +0300

    initial commit

---
+++ auth/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+const app = express();
+app.use(json());
+
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('working');
+});
+
+app.listen(3000, () => console.log('Listening on port 3000'));
