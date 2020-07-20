eab2d67ce7c6e9cc75deab7866c10f00d64cd2dd
ormeiry <ormeiry2411@gmail.com>
2020-07-20 19:43:02 +0300

    added error handling initial logic, created custom errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -15,13 +17,14 @@ router.post(
   (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
 
     const { email, password } = req.body;
 
     // temp
     console.log('creating a user');
+    throw new DatabaseConnectionError();
     res.send({});
   }
 );
