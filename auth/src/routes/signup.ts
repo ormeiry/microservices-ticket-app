90e8d08909220c62454088347ba1dc259a591667
ormeiry <ormeiry2411@gmail.com>
2020-07-22 13:46:16 +0300

    init session-cookie with jwt, testing the flow

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
 import { User } from '../models/user';
@@ -30,6 +32,21 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Gen JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      'asdf'
+    );
+
+    // Store it on session cookie
+    req.session = {
+      jwt: userJwt,
+    };
+
     res.status(201).send(user);
   }
 );
