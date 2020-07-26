3d95a0ab2024b96bf0966ead1a7765747ca041da
ormeiry <ormeiry2411@gmail.com>
2020-07-26 18:24:05 +0300

    setup testing with jest, config testing to work well with ts. wrote tests for all auth routes

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,25 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('responds with details about the current user', async () => {
+  const cookie = await global.signup();
+
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .set('Cookie', cookie)
+    .send()
+    .expect(200);
+
+  console.log(response.body);
+});
+
+it('responds with null if not authenticated', async () => {
+  const response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser).toEqual(null);
+
+  console.log(response.body);
+});
---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,66 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns a 201 on successful signup', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+});
+
+it('returns a 400 with invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'p',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+  return request(app).post('/api/users/signup').send({}).expect(400);
+});
+
+it('dissallows duplicate emails', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('sets a cookie after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
