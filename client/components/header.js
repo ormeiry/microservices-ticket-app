639936ec0e5a584f17cf085a31133206616a1949
ormeiry <ormeiry2411@gmail.com>
2020-07-29 22:51:31 +0300

    Built the auth pages, setup custom request hook, setup getinitailporps function, built the header component

---
+++ client/components/header.js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+
+export default ({ currentUser }) => {
+  const Links = [
+    !currentUser && { label: 'Sign Up', href: '/auth/signup' },
+    !currentUser && { label: 'Sign In', href: '/auth/signin' },
+    currentUser && { label: 'Sign Out', href: '/auth/signout' },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href} className='nav-item'>
+          <Links href={href}>
+            <a className='nav-link'>{label}</a>
+          </Links>
+        </li>
+      );
+    });
+
+  return (
+    <nav className='navbar navbar-light bg-light'>
+      <Link href='/'>
+        <a className='navbar-brand'>GitTix</a>
+      </Link>
+
+      <div className='d-flex justify-content-end'>
+        <ul className='nav d-flex align-items-center'>{Links}</ul>
+      </div>
+    </nav>
+  );
+};
